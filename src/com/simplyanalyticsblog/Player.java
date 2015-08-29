5447747d4f071285c78abb31a1854cf1e959bad3
Gianluca Rossi <grossi@quantcast.com>
2015-08-29 14:22:14 +0100

    Add single player logic to makeSecondGuess

--- src/com/simplyanalyticsblog/Player.java
+++ src/com/simplyanalyticsblog/Player.java
@@ -1,5 +1,7 @@
 package com.simplyanalyticsblog;
 
+import com.sun.javafx.tk.ScreenConfigurationAccessor;
+
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -24,7 +26,7 @@ public class Player {
         } else {
 
             Scanner reader = new Scanner(System.in);
-            System.out.println("What is your initial choice? [A, B, C]");
+            System.out.println("What is your initial choice? [A/B/C]");
             String choice = reader.next();
             int index = "abc".indexOf(choice.toLowerCase());
             doors.get(index).setPlayerGuess(true);
@@ -35,28 +37,44 @@ public class Player {
 
     public void makeSecondGuess(ArrayList<Door> doors) {
 
-        // player decides if switching door
-        String options = "yn";
-        Random randomGenerator = new Random();
-        int index = randomGenerator.nextInt(options.length());
-        char decision = options.charAt(index);
+        if (!this.realPlayer) {
 
-        System.out.println("Does the player switch door?: " + decision);
+            String options = "yn";
+            Random randomGenerator = new Random();
+            int index = randomGenerator.nextInt(options.length());
+            char decision = options.charAt(index);
 
-        char y = 'y';
+            System.out.println("Does the player switch door?: " + decision);
 
-        if (y == decision) {
-            for (Door door : doors) {
-                if (door.isOpenDoor()) {
-                    continue;
-                } else if (door.isPlayerGuess()) {
-                    door.setPlayerGuess(false);
-                } else {
-                    door.setPlayerGuess(true);
+            char y = 'y';
+            if (y == decision) {
+                for (Door door : doors) {
+                    if (door.isOpenDoor()) {
+                        continue;
+                    } else if (door.isPlayerGuess()) {
+                        door.setPlayerGuess(false);
+                    } else {
+                        door.setPlayerGuess(true);
+                    }
+                }
+            }
+        } else {
+
+            Scanner reader = new Scanner(System.in);
+            System.out.println("Would you like to change your initial choice? [Y/N]");
+            String choice = reader.next();
+
+            if ("y".equals(choice.toLowerCase())) {
+                for (Door door : doors) {
+                    if (door.isOpenDoor()) {
+                        continue;
+                    } else if (door.isPlayerGuess()) {
+                        door.setPlayerGuess(false);
+                    } else {
+                        door.setPlayerGuess(true);
+                    }
                 }
             }
         }
-
-        // TODO: add logic for single player mode
     }
 }
